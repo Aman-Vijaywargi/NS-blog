0c2b93a4a5e0d8f6d07e7f7e53f3c7e63d023761
Gowtham Chowdary <42214663+GowthamChowdary@users.noreply.github.com>
2021-03-27 22:43:27 +0530

    added 2d arrays

--- DSA/Arrays.py
+++ DSA/Arrays.py
@@ -231,3 +231,315 @@ arr = list(map(int,input().split()))
 n = len(arr) 
 print(countBuildings(arr, n))
 
+# 2D ARRAYS
+
+# Good cells
+
+# standard input/output: 2s/128000 kB
+
+# Given a boolean Matrix of size N*M, A cell of the matrix is called "Good" if it is completely surrounded by the cells containing '1' i.e. each adjacent cell (which shares a common edge) contains '1'. Your task is to find the number of such cells.
+
+# See the below example for a better understanding
+# Input
+# First line of input contains two space- separated integers N and M. Next N lines of input contain M space- separated integers depicting the values of the matrix.
+
+# Constraints:-
+# 3 <= N, M <= 500
+# 0 <= Matrix[][] <= 1
+# Output
+# Print the number of good cells.
+
+column,row=map(int,input().split())
+a=[]
+s=0
+for z in range(column):
+	a.append(list(map(int,input().split())))
+for i in range(1,len(a)):
+	for j in range(1,len(a[i])):
+		#print(a[i][j])
+		try:
+			if a[i+1][j]==1 and a[i-1][j]==1 and a[i][j+1]==1 and a[i][j-1]==1:
+				s+=1
+		except:
+			continue
+print(s)
+
+# Alternate Matrix Addition
+
+# standard input/output: 2s/128000 kB
+
+# You are given a chessboard of size N x N, where the top left square is black. Each square contains a value. Find the sum of values of all black square and all white squares.
+
+# Remember that in a chessboard black and white squares alternate.
+# Input
+# The first line of input will be the N size of the matrix. Then next N lines will consist of elements of the matrix. Each row will contain N elements since it is a square matrix.
+
+# Constraints:-
+# 1 <= N <= 800
+# 1 <= Matrix[i][j] <= 100000
+# Output
+# Print two lines, first line containing the sum of black squares and second line containing the sum of white squares.
+
+#code: c++
+
+#include <bits/stdc++.h> // header file includes every Standard library
+# using namespace std;
+
+# int main() {
+#     	int n;
+#         cin>>n;
+#         long black = 0, white = 0;
+#         for (int i = 0; i < n; i++) {
+#             for (int j = 0; j < n; j++) {
+#                 long x;
+#                 cin>>x;
+#                 if ((i + j) % 2 == 0) {
+#                     black += x;
+#                 } else {
+#                     white += x;
+#                 }
+#             }
+#         }
+#         cout<<black<<"\n"<<white;
+# 	return 0;
+
+# }
+
+# Simple-Transpose
+
+# standard input/output: 2s/128000 kB
+
+# You are given a NXN matrix. You need to find the transpose of the matrix.
+# The matrix is of form:
+# a b c ...
+# d e f ...
+# g h i ...
+# ...........
+# There are N elements in each row.
+# Input
+# The first line of the input contains an integer N denoting the size of the square matrix.
+# The next N lines contain N single-spaced integers.
+
+# Constraints
+# 1 <= N <= 100
+# 1 <=Ai <= 100000
+# Output
+# Output the transpose of the matrix in similar format as that of the input.
+
+#Code: Java
+
+# import java.io.*; // for handling input/output
+# import java.util.*; // contains Collections framework
+
+# // don't change the name of this class
+# // you can add inner classes if needed
+# class Main {
+# 	public static void main (String[] args) {
+#                       Scanner sc = new Scanner(System.in);
+# 					  int[][] arr = new int[1000][1000];
+# 					  int n = sc.nextInt();
+# 					  for(int i=0;i<n;i++){
+# 						  for(int j=0;j<n;j++){
+# 							  arr[i][j] = sc.nextInt();
+# 						  }
+# 					}
+# 					int temp = 0;
+# 					for(int i=0;i<n;i++){
+# 						for(int j=0;j<n;j++){
+# 							if (i<j){
+# 								temp= arr[i][j]; // 8
+# 								arr[i][j] = arr[j][i]; // 7
+# 								arr[j][i] = temp; // 8
+# 							}
+# 						}
+# 					}
+# 					 for(int i=0;i<n;i++){
+# 						  for(int j=0;j<n;j++){
+# 							  System.out.print(arr[i][j] + " ");
+# 						  }
+# 						  System.out.println();
+# 					 }
+						  
+
+# 	}
+# }
+
+# A Boolean Matrix Problem
+
+# standard input/output: 2s/128000 kB
+
+# You are given a matrix Mat of m rows and n columns. The matrix is boolean so the elements of the matrix can only be either 0 or 1.
+# Now, if any row of the matrix contains a 1, then you need to fill that whole row with 1. After doing the mentioned operation, you need to print the modified matrix.
+# Input
+# The first line of input contains T denoting the number of testcases. T testcases follow.
+# The first line of each testcase contains m and n denoting number of rows and number of columns.
+# Then next m lines contain n elements denoting the elements of the matrix.
+
+# Constraints:
+# 1 <= T <= 20
+# 1 <= m, n <= 700
+# Mat[I][j] ∈ {0,1}
+# Output
+# For each testcase, in a new line, print the modified matrix.
+
+#code: java
+	# import java.io.*; // for handling input/output
+	# import java.util.*; // contains Collections framework
+
+	# // don't change the name of this class
+	# // you can add inner classes if needed
+	# class Main {
+	# 	public static void main (String[] args) throws IOException {
+	# 					// Your code here
+		
+	# 		BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+	# 		int test = Integer.parseInt(rd.readLine());
+
+	# 		while(test-- > 0){
+	# 			String[] rowcol = rd.readLine().split(" ");
+	# 			int rows = Integer.parseInt(rowcol[0]);
+	# 			int cols = Integer.parseInt(rowcol[1]);
+
+	# 			int[][]arr = new int[rows][cols];
+	# 			for(int r=0; r<rows; r++){
+	# 				String[] rowStr = rd.readLine().split(" ");
+	# 				for(int c=0; c<cols; c++){
+	# 					arr[r][c] = Integer.parseInt(rowStr[c]);
+	# 				}
+	# 			}
+
+	# 			for(int r=0; r<rows; r++){
+	# 				for(int c=0; c<cols; c++){
+	# 					if(arr[r][c] == 1){
+	# 						arr[r][0] = 1;
+	# 						break;
+	# 					}
+	# 				}
+	# 			}
+
+	# 			for(int r=0; r<rows; r++){
+	# 				if(arr[r][0] == 1){
+	# 					for(int c=0; c<cols; c++){
+	# 						arr[r][c] = 1;
+	# 					}
+	# 				}
+	# 			}
+
+	# 			for(int r=0; r<rows; r++){
+	# 				for(int c=0; c<cols; c++){
+	# 					System.out.print(arr[r][c] +" ");
+	# 				}
+	# 				System.out.println();
+	# 			}
+
+	# 		}
+	# 	}
+	# }
+
+# Transpose of a matrix
+#code : java
+#  import java.io.*; // for handling input/output
+# import java.util.*; // contains Collections framework
+
+# // don't change the name of this class
+# // you can add inner classes if needed
+# class Main {
+# 	public static void main (String[] args) {
+#                       Scanner sc = new Scanner(System.in);
+					  
+# 					  int n = sc.nextInt();
+# 					  int [][]arr = new int[n][n];
+# 					  for(int i=0;i<n;i++){
+# 						  for(int j=0;j<n;j++){
+# 							arr[i][j] = i+j;
+# 						  }
+# 					  }
+# 					  for(int i=0;i<n;i++){
+# 						  for(int j=0;j<n;j++){
+# 							System.out.print(arr[i][j]+ " ");
+# 						  }
+# 						  System.out.println();
+# 					  }
+
+# 	}
+# }
+
+# Boundary Traversal of Matrix
+#code: Java
+# import java.io.*; // for handling input/output
+# import java.util.*; // contains Collections framework
+
+# // don't change the name of this class
+# // you can add inner classes if needed
+# class Main {
+#     public static void main(String[] args)throws IOException {
+        
+#         // Your code here
+#         Scanner in = new Scanner(System.in);
+#         int t = in.nextInt();
+#         for (int k = 0; k < t; k++) {
+#             int m = in.nextInt();
+#             int n = in.nextInt();
+#             int[][] arr = new int[m][n];
+#             for (int i = 0; i < m; i++) {
+#                 for (int j = 0; j < n; j++) {
+#                     arr[i][j] = in.nextInt();
+#                 }
+#             }
+#             if (m == 1) {
+#                 for (int i = 0; i < n; i++) {
+#                     System.out.print(arr[0][i] + " ");
+#                 }
+#             } else if (n == 1) {
+#                 for (int j = 0; j < m; j++) {
+#                     System.out.print(arr[j][0] + " ");
+#                 }
+#             } else {
+#                 for (int j = 0; j < n; j++) {
+#                     System.out.print(arr[0][j] + " ");
+#                 }
+#                 for (int i = 1; i < m - 1; i++) {
+#                     System.out.print(arr[i][n - 1] + " ");
+#                 }
+#                 for (int j = n; j > 0; j--) {
+#                     System.out.print(arr[m - 1][j - 1] + " ");
+#                 }
+#                 for (int i = m - 1; i > 1; i--) {
+#                     System.out.print(arr[i - 1][0] + " ");
+#                 }
+#             }
+#             System.out.println();
+#         }
+#     }
+# }
+# Print the Matrix
+n=int(input())
+a=[]
+for i in range(n):
+	x=str(input())
+	a.append(x)
+for j in a:
+	print(j)
+#Diagonal Sum
+MAX = 100
+
+def printDiagonalSums(mat, n): 
+
+	principal = 0
+	secondary = 0
+	for i in range(0, n): 
+		for j in range(0, n): 
+			if (i == j): 
+				principal += mat[i][j] 
+			if ((i + j) == (n - 1)): 
+				secondary += mat[i][j] 
+		
+	print(principal,secondary) 
+
+n=int(input())	
+a=[]
+for i in range(n):
+	a.append(list(map(int,input().split())))
+
+printDiagonalSums(a, n)
+
